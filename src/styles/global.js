e02ee7821b90c1810ea7393cb0586aa912cafa18
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-27 16:49:06 -0300

    :art: style: updated global styles
    changed opacity and background color

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,37 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-:root {
-    --green-bold: #01ff2c;
-    --color-green: #49d260;
-}
 
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Arial, Helvetica, sans-serif;
-    color: var(--color-green);
+    font-family: Lora,Arial, Helvetica, sans-serif;
+    color: #fff;
 }
 
 a {
     text-decoration: none;
-    color: #fff;
+}
+
+p {
+    opacity: 0.8;
 }
 
 body {
     color: #fff;
-    background-color: #000400;
+    background-color: #00001f;
 }
 
-.division {
-    width: 100px;
-    height: 5px;
-    margin: 35px 0px;
-    border-radius: 15px;
-
-    background: #000000;
-    background: -webkit-linear-gradient(to right, #000000, #0f9b0f);
-    background: linear-gradient(to right, #000000, #0f9b0f);
-}
+h2 {
+        margin-bottom: 20px;
+        font-size: 42px;
+        margin-bottom: 20px;
+    }
 `;
