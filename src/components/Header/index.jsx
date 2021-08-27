48c8c258a59c945a64fa1bfee677ceb99473b321
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-27 16:53:36 -0300

    :wrench: feat: implementation react-scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { HeaderStyle } from './styles';
+import { Link } from 'react-scroll';
 
 export default function Header() {
     return (
@@ -7,19 +8,56 @@ export default function Header() {
             <h1>&lt;Portfolio /&gt;</h1>
             <nav>
                 <ul>
-                    <li>Projects</li>
-                    <li>Technologies</li>
-                    <li>About</li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Projects"
+                            spy={true}
+                            smooth={true}
+                            duration={1300}
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="Technologies"
+                            spy={true}
+                            smooth={true}
+                            duration={2000}
+                        >
+                            Technologies
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            activeClass="active"
+                            to="About"
+                            spy={true}
+                            smooth={true}
+                            duration={2000}
+                        >
+                            About
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <div className="socialMedias">
                 {' '}
                 <p>
-                    <AiFillGithub />
+                    <a
+                        href="https://github.com/Wesley-wsl"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <AiFillGithub />
+                    </a>
                 </p>
                 <p>
-                    {' '}
-                    <AiFillLinkedin />
+                    <a href="#">
+                        <AiFillLinkedin />
+                    </a>
                 </p>
             </div>
         </HeaderStyle>
