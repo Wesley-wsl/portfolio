ffdc4470890340e2e372a9434010a7d3eab01dcd
Wesley Westelley <wesleywestelley@gmail.com>
2021-08-28 15:47:48 -0300

    :art: style: created style for header mobile

---
+++ src/components/HeaderMobile/styles.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.header`
+    display: flex;
+    justify-content: space-between;
+    max-width: 450px;
+    margin: 0 auto;
+    h1 {
+        cursor: pointer;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    padding: 15px;
+
+    @media (min-width: 651px) {
+        display: none;
+    }
+`;
+
+export const Menu = styled.div`
+    cursor: pointer;
+    position: relative;
+    div {
+        height: 2px;
+        background-color: #fff;
+        width: 30px;
+        margin-bottom: 6px;
+    }
+`;
+
+export const NavBarMobile = styled.nav`
+    display: ${props => (props.active ? 'block' : 'none')};
+    background-color: #000039;
+    border-radius: 10px;
+    position: absolute;
+    top: 40px;
+    right: 10px;
+    z-index: 1;
+    animation: appear 0.2s linear;
+    li {
+        list-style: none;
+        padding: 15px;
+
+        display: flex;
+        gap: 10px;
+        svg {
+            font-size: 20px;
+        }
+        a {
+            cursor: pointer;
+        }
+    }
+
+    @keyframes appear {
+        0% {
+            opacity: 0;
+            transform: translateY(-50px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0px);
+        }
+    }
+`;
