b86497f198b49b329eed10c3db107a5eede68b42
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-23 00:48:43 -0300

    :art: style: change colors for ProjectCard component

--- src/components/ProjectCard/styles.ts
+++ src/components/ProjectCard/styles.ts
@@ -1,46 +1,49 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-    max-width: 350px;
+    max-width: 450px;
     margin: 0 auto;
     border-radius: 10px;
-    box-shadow: 1px 1px 10px 0px #1488cc;
+    box-shadow: 1px 1px 10px 0px #000;
+    margin-bottom: 30px;
+    transition: all 0.2s ease-in-out;
     img {
         width: 350px;
         height: 200px;
+        object-fit: cover;
     }
 
     h3 {
-        font-size: 30px;
+        font-size: var(--fz-sm-heading);
         margin: 0 auto;
         display: flex;
         justify-content: center;
+        letter-spacing: 4px;
+        margin-top: 3px;
     }
 
     .division {
         margin: 15px auto;
-        height: 3px;
-        width: 60px;
-        background: #005aa7;
     }
 
     p {
         max-width: 300px;
         text-align: justify;
         margin: 5px auto;
-        text-shadow: 0px 0px 3px #005aa7;
+        text-shadow: 0px 0px 3px var(--background);
     }
 
-    .labelStack {
-        text-align: center;
-        margin: 10px auto;
+    .description {
+        height: 120px;
+        overflow: hidden;
     }
 
-    .stacks {
-        display: flex;
-        p {
-            max-width: 350px;
-            text-align: center;
+    @media (max-width: 417px) {
+        width: 270px;
+        padding: 20px;
+        img {
+            width: 200px;
+            height: 100px;
         }
     }
 `;
@@ -48,12 +51,16 @@ export const Card = styled.div`
 export const Demo = styled.div`
     display: flex;
     justify-content: space-around;
-    margin: 10px 0;
+    margin: 30px 0;
 
     a {
-        background-color: #005;
+        background-color: #111;
         padding: 7px;
         border-radius: 10px;
         margin-bottom: 10px;
+        transition: all 0.2s linear;
+        &:hover {
+            transform: translateY(-4px);
+        }
     }
 `;
