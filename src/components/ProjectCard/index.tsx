3eb819b007d138a1b21ecef5b05e23a9aec08bee
Wesley Westelley <wesleywestelley@gmail.com>
2022-01-23 00:48:11 -0300

    :recycle: refactor: remove fragment and stack props

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,37 +1,33 @@
 import { IProjects } from '../../@types';
+import { Division } from '../../pages/Home/styles';
 import { Card, Demo } from './styles';
+
+import 'aos/dist/aos.css';
 
 export default function ProjectCard({
     img,
     name,
     description,
-    stacks,
     source,
     live,
 }: IProjects) {
     return (
-        <>
-            <Card>
-                <img src={img} alt="Project image" />
+        <Card data-aos="fade-up">
+            <img src={img} alt="Project image" />
 
-                <h3>{name}</h3>
-                <div className="division" />
+            <h3>{name}</h3>
+            <Division className="division" />
 
-                <p>{description}</p>
-                <p className="labelStack">Stacks</p>
-                <div className="stacks">
-                    <p>{stacks}</p>
-                </div>
+            <p className="description">{description}</p>
 
-                <Demo>
-                    <a href={source} target="_blank" rel="noreferrer">
-                        Code
-                    </a>
-                    <a href={live} target="_blank" rel="noreferrer">
-                        Live
-                    </a>
-                </Demo>
-            </Card>
-        </>
+            <Demo>
+                <a href={source} target="_blank" rel="noreferrer">
+                    Code
+                </a>
+                <a href={live} target="_blank" rel="noreferrer">
+                    Live
+                </a>
+            </Demo>
+        </Card>
     );
 }
